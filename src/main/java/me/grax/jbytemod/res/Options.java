d3d84f441a2c496385bb10205bba7d2501e6baaf
GraxCode <grax@graxco.de>
2018-01-20 15:11:28 +0100

    options bug fix

--- src/main/java/me/grax/jbytemod/res/Options.java
+++ src/main/java/me/grax/jbytemod/res/Options.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 import me.grax.jbytemod.JByteMod;
 import me.grax.jbytemod.res.Option.Type;
 import me.grax.jbytemod.utils.ErrorDisplay;
@@ -16,6 +18,9 @@ public class Options {
   private static final File propFile = new File("jbytemod.cfg");
 
   public List<Option> bools = new ArrayList<>();
+  public List<Option> defaults = Arrays.asList(new Option("sort_methods", false, Type.BOOLEAN), new Option("tree_search_sel", false, Type.BOOLEAN),
+      new Option("load_rt_startup", false, Type.BOOLEAN), new Option("compute_maxs", true, Type.BOOLEAN),
+      new Option("primary_color", "#557799", Type.STRING, "color_group"), new Option("secondary_color", "#995555", Type.STRING, "color_group"));
 
   public Options() {
     if (propFile.exists()) {
@@ -30,6 +35,16 @@ public class Options {
             System.err.println("Couldn't parse line: " + l);
           }
         });
+        for(int i = 0; i < bools.size(); i++) {
+          Option o1 =  bools.get(i);
+          Option o2 =  defaults.get(i);
+          if(o1 == null || o2 == null || !o1.getName().equals(o2)) {
+            JOptionPane.showMessageDialog(null, "Corrupt option file, rewriting");
+            this.initWithDefaults();
+            this.save();
+            return;
+          }
+        }
         if (bools.isEmpty()) {
           System.err.println("Couldn't read file, probably empty");
           this.initWithDefaults();
@@ -46,9 +61,8 @@ public class Options {
   }
 
   private void initWithDefaults() {
-    bools = Arrays.asList(new Option("sort_methods", false, Type.BOOLEAN), new Option("tree_search_sel", false, Type.BOOLEAN),
-        new Option("load_rt_startup", false, Type.BOOLEAN), new Option("primary_color", "#557799", Type.STRING, "color_group"),
-        new Option("secondary_color", "#995555", Type.STRING, "color_group"));
+    bools = new ArrayList<>();
+    bools.addAll(defaults);
   }
 
   public void save() {
@@ -66,6 +80,14 @@ public class Options {
   }
 
   public Option get(String name) {
+    for (Option o : bools) {
+      if (o.getName().equalsIgnoreCase(name)) {
+        return o;
+      }
+    }
+    JOptionPane.showMessageDialog(null, "Missing option: " + name + "\nRewriting your config file!");
+    this.initWithDefaults();
+    this.save();
     for (Option o : bools) {
       if (o.getName().equalsIgnoreCase(name)) {
         return o;
