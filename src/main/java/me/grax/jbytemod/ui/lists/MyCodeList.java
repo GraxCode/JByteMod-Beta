205e7c27befebec1067e7c04a80a17ccbd4f73c4
GraxCode <grax@graxco.de>
2018-01-21 11:36:58 +0100

    list additions

--- src/main/java/me/grax/jbytemod/ui/lists/MyCodeList.java
+++ src/main/java/me/grax/jbytemod/ui/lists/MyCodeList.java
@@ -20,6 +20,7 @@ import javax.swing.JList;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.KeyStroke;
+import javax.swing.ListModel;
 import javax.swing.SwingUtilities;
 import javax.swing.event.PopupMenuEvent;
 import javax.swing.event.PopupMenuListener;
@@ -27,6 +28,7 @@ import javax.swing.event.PopupMenuListener;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.tree.JumpInsnNode;
 import org.objectweb.asm.tree.LabelNode;
 import org.objectweb.asm.tree.MethodNode;
 
@@ -53,223 +55,17 @@ public class MyCodeList extends JList<InstrEntry> {
     this.addMouseListener(new MouseAdapter() {
       public void mousePressed(MouseEvent e) {
         InstrEntry entry = (InstrEntry) MyCodeList.this.getSelectedValue();
-        List<InstrEntry> selected = MyCodeList.this.getSelectedValuesList();
         if (entry == null) {
-          JPopupMenu menu = new JPopupMenu();
-          if (currentMethod != null) {
-            JMenuItem add = new JMenuItem("Add");
-            add.addActionListener(new ActionListener() {
-              public void actionPerformed(ActionEvent e) {
-                try {
-                  EditDialogue.createInsertInsnDialog(currentMethod, null, true);
-                } catch (Exception e1) {
-                  new ErrorDisplay(e1);
-                }
-
-              }
-            });
-            menu.add(add);
-          } else if (currentClass != null) {
-            JMenuItem add = new JMenuItem("Add");
-            add.addActionListener(new ActionListener() {
-              public void actionPerformed(ActionEvent e) {
-                try {
-                  FieldNode fn = new FieldNode(1, "", "", "", null);
-                  EditDialogueSpecial.createInsertDialogue(null, fn);
-                  if (fn.signature.isEmpty()) {
-                    fn.signature = null;
-                  }
-                  currentClass.fields.add(fn);
-                } catch (Exception e1) {
-                  new ErrorDisplay(e1);
-                }
-                MyCodeList.this.loadFields(currentClass);
-              }
-            });
-            menu.add(add);
-          }
-          menu.show(jam, (int) jam.getMousePosition().getX(), (int) jam.getMousePosition().getY());
+          createPopupForEmptyList(jam);
           return;
         }
         MethodNode mn = entry.getMethod();
         if (SwingUtilities.isRightMouseButton(e)) {
           AbstractInsnNode ain = entry.getInstr();
           if (mn != null) {
-            if (selected.size() > 1) {
-              JPopupMenu menu = new JPopupMenu();
-              JMenuItem remove = new JMenuItem("Remove All");
-              remove.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                  for (InstrEntry sel : selected) {
-                    mn.instructions.remove(sel.getInstr());
-                  }
-                  OpUtils.clearLabelCache();
-                  MyCodeList.this.loadInstructions(mn);
-                }
-              });
-              menu.add(remove);
-              menu.addPopupMenuListener(new PopupMenuListener() {
-                public void popupMenuCanceled(PopupMenuEvent popupMenuEvent) {
-                  MyCodeList.this.setFocusable(true);
-                }
-
-                public void popupMenuWillBecomeInvisible(PopupMenuEvent popupMenuEvent) {
-                  MyCodeList.this.setFocusable(true);
-                }
-
-                public void popupMenuWillBecomeVisible(PopupMenuEvent popupMenuEvent) {
-                  MyCodeList.this.setFocusable(false);
-                }
-              });
-              menu.show(jam, (int) jam.getMousePosition().getX(), (int) jam.getMousePosition().getY());
-            } else {
-              JPopupMenu menu = new JPopupMenu();
-              JMenuItem insertBefore = new JMenuItem("Insert before");
-              insertBefore.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                  try {
-                    InsnEditDialogue.createInsertInsnDialog(mn, ain, false);
-                    OpUtils.clearLabelCache();
-                  } catch (Exception e1) {
-                    new ErrorDisplay(e1);
-                  }
-                }
-              });
-              menu.add(insertBefore);
-              JMenuItem insert = new JMenuItem("Insert after");
-              insert.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                  try {
-                    InsnEditDialogue.createInsertInsnDialog(mn, ain, true);
-                    OpUtils.clearLabelCache();
-                  } catch (Exception e1) {
-                    new ErrorDisplay(e1);
-                  }
-                }
-              });
-              menu.add(insert);
-
-              if (EditDialogue.canEdit(ain)) {
-                JMenuItem edit = new JMenuItem("Edit");
-                edit.addActionListener(new ActionListener() {
-                  public void actionPerformed(ActionEvent e) {
-                    try {
-//                      EditDialogue.createEditInsnDialog(mn, ain);
-                      new InsnEditDialogue(mn, ain).open();
-                    } catch (Exception e1) {
-                      new ErrorDisplay(e1);
-                    }
-                  }
-                });
-                menu.add(edit);
-              }
-              JMenuItem duplicate = new JMenuItem("Duplicate");
-              duplicate.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                  try {
-                    if (ain instanceof LabelNode) {
-                      mn.instructions.insert(ain, new LabelNode());
-                      OpUtils.clearLabelCache();
-                    } else {
-                      mn.instructions.insert(ain, ain.clone(new HashMap<>()));
-                    }
-                    MyCodeList.this.loadInstructions(mn);
-
-                  } catch (Exception e1) {
-                    new ErrorDisplay(e1);
-                  }
-                }
-              });
-              menu.add(duplicate);
-              JMenuItem up = new JMenuItem("Move up");
-              up.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                  AbstractInsnNode node = ain.getPrevious();
-                  mn.instructions.remove(node);
-                  mn.instructions.insert(ain, node);
-                  OpUtils.clearLabelCache();
-                  MyCodeList.this.loadInstructions(mn);
-                }
-              });
-              menu.add(up);
-              JMenuItem down = new JMenuItem("Move down");
-              down.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                  AbstractInsnNode node = ain.getNext();
-                  mn.instructions.remove(node);
-                  mn.instructions.insertBefore(ain, node);
-                  OpUtils.clearLabelCache();
-                  MyCodeList.this.loadInstructions(mn);
-                }
-              });
-              menu.add(down);
-              JMenuItem remove = new JMenuItem("Remove");
-              remove.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                  mn.instructions.remove(ain);
-                  OpUtils.clearLabelCache();
-                  MyCodeList.this.loadInstructions(mn);
-                }
-              });
-              menu.add(remove);
-              menu.addPopupMenuListener(new PopupMenuListener() {
-                public void popupMenuCanceled(PopupMenuEvent popupMenuEvent) {
-                  MyCodeList.this.setFocusable(true);
-                }
-
-                public void popupMenuWillBecomeInvisible(PopupMenuEvent popupMenuEvent) {
-                  MyCodeList.this.setFocusable(true);
-                }
-
-                public void popupMenuWillBecomeVisible(PopupMenuEvent popupMenuEvent) {
-                  MyCodeList.this.setFocusable(false);
-                }
-              });
-              menu.show(jam, (int) jam.getMousePosition().getX(), (int) jam.getMousePosition().getY());
-            }
+            rightClickMethod(jam, mn, ain, MyCodeList.this.getSelectedValuesList());
           } else {
-            FieldEntry fle = (FieldEntry) entry;
-            ClassNode cn = fle.getCn();
-            JPopupMenu menu = new JPopupMenu();
-            JMenuItem edit = new JMenuItem("Edit");
-            edit.addActionListener(new ActionListener() {
-              public void actionPerformed(ActionEvent e) {
-                try {
-                  EditDialogue.createEditFieldDialog(cn, fle.getFn());
-                } catch (Exception e1) {
-                  new ErrorDisplay(e1);
-                }
-                MyCodeList.this.loadFields(cn);
-              }
-            });
-            menu.add(edit);
-            JMenuItem remove = new JMenuItem("Remove");
-            remove.addActionListener(new ActionListener() {
-              public void actionPerformed(ActionEvent e) {
-                cn.fields.remove(fle.getFn());
-                MyCodeList.this.loadFields(cn);
-              }
-            });
-            menu.add(remove);
-            JMenuItem add = new JMenuItem("Insert");
-            add.addActionListener(new ActionListener() {
-              public void actionPerformed(ActionEvent e) {
-                try {
-                  FieldNode fn = new FieldNode(1, "", "", "", null);
-                  if (EditDialogueSpecial.createInsertDialogue(null, fn)) {
-                    if (fn.signature.isEmpty()) {
-                      fn.signature = null;
-                    }
-                    cn.fields.add(fn);
-                  }
-                } catch (Exception e1) {
-                  new ErrorDisplay(e1);
-                }
-                MyCodeList.this.loadFields(cn);
-              }
-            });
-            menu.add(add);
-            menu.show(jam, (int) jam.getMousePosition().getX(), (int) jam.getMousePosition().getY());
+            rightClickField(jam, (FieldEntry) entry);
           }
         }
       }
@@ -284,6 +80,231 @@ public class MyCodeList extends JList<InstrEntry> {
       @Override
       public void actionPerformed(ActionEvent e) {
         new JSearch(MyCodeList.this).setVisible(true);
+      }
+    });
+  }
+
+  protected void rightClickField(JByteMod jbm, FieldEntry fle) {
+    ClassNode cn = fle.getCn();
+    JPopupMenu menu = new JPopupMenu();
+    JMenuItem edit = new JMenuItem("Edit");
+    edit.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        try {
+          EditDialogue.createEditFieldDialog(cn, fle.getFn());
+        } catch (Exception e1) {
+          new ErrorDisplay(e1);
+        }
+        MyCodeList.this.loadFields(cn);
+      }
+    });
+    menu.add(edit);
+    JMenuItem remove = new JMenuItem("Remove");
+    remove.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        cn.fields.remove(fle.getFn());
+        MyCodeList.this.loadFields(cn);
+      }
+    });
+    menu.add(remove);
+    JMenuItem add = new JMenuItem("Insert");
+    add.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        try {
+          FieldNode fn = new FieldNode(1, "", "", "", null);
+          if (EditDialogueSpecial.createInsertDialogue(null, fn)) {
+            if (fn.signature.isEmpty()) {
+              fn.signature = null;
+            }
+            cn.fields.add(fn);
+          }
+        } catch (Exception e1) {
+          new ErrorDisplay(e1);
+        }
+        MyCodeList.this.loadFields(cn);
+      }
+    });
+    menu.add(add);
+    menu.show(jbm, (int) jbm.getMousePosition().getX(), (int) jbm.getMousePosition().getY());
+  }
+
+  protected void rightClickMethod(JByteMod jbm, MethodNode mn, AbstractInsnNode ain, List<InstrEntry> selected) {
+    if (selected.size() > 1) {
+      JPopupMenu menu = new JPopupMenu();
+      JMenuItem remove = new JMenuItem("Remove All");
+      remove.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          for (InstrEntry sel : selected) {
+            mn.instructions.remove(sel.getInstr());
+          }
+          OpUtils.clearLabelCache();
+          MyCodeList.this.loadInstructions(mn);
+        }
+      });
+      menu.add(remove);
+      addPopupListener(menu);
+      menu.show(jbm, (int) jbm.getMousePosition().getX(), (int) jbm.getMousePosition().getY());
+    } else {
+      JPopupMenu menu = new JPopupMenu();
+      JMenuItem insertBefore = new JMenuItem("Insert before");
+      insertBefore.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          try {
+            InsnEditDialogue.createInsertInsnDialog(mn, ain, false);
+            OpUtils.clearLabelCache();
+          } catch (Exception e1) {
+            new ErrorDisplay(e1);
+          }
+        }
+      });
+      menu.add(insertBefore);
+      JMenuItem insert = new JMenuItem("Insert after");
+      insert.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          try {
+            InsnEditDialogue.createInsertInsnDialog(mn, ain, true);
+            OpUtils.clearLabelCache();
+          } catch (Exception e1) {
+            new ErrorDisplay(e1);
+          }
+        }
+      });
+      menu.add(insert);
+
+      if (EditDialogue.canEdit(ain)) {
+        JMenuItem edit = new JMenuItem("Edit");
+        edit.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            try {
+              new InsnEditDialogue(mn, ain).open();
+            } catch (Exception e1) {
+              new ErrorDisplay(e1);
+            }
+          }
+        });
+        menu.add(edit);
+      }
+      if (ain instanceof JumpInsnNode) {
+        JMenuItem edit = new JMenuItem("Jump to Label");
+        edit.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            JumpInsnNode jin = (JumpInsnNode) ain;
+            ListModel<InstrEntry> model = getModel();
+            for (int i = 0; i < model.getSize(); i++) {
+              InstrEntry sel = model.getElementAt(i);
+              if (sel.getInstr().equals(jin.label)) {
+                setSelectedIndex(i);
+                ensureIndexIsVisible(i);
+                break;
+              }
+            }
+          }
+        });
+        menu.add(edit);
+      }
+      JMenuItem duplicate = new JMenuItem("Duplicate");
+      duplicate.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          try {
+            if (ain instanceof LabelNode) {
+              mn.instructions.insert(ain, new LabelNode());
+              OpUtils.clearLabelCache();
+            } else {
+              mn.instructions.insert(ain, ain.clone(new HashMap<>()));
+            }
+            MyCodeList.this.loadInstructions(mn);
+
+          } catch (Exception e1) {
+            new ErrorDisplay(e1);
+          }
+        }
+      });
+      menu.add(duplicate);
+      JMenuItem up = new JMenuItem("Move up");
+      up.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          AbstractInsnNode node = ain.getPrevious();
+          mn.instructions.remove(node);
+          mn.instructions.insert(ain, node);
+          OpUtils.clearLabelCache();
+          MyCodeList.this.loadInstructions(mn);
+        }
+      });
+      menu.add(up);
+      JMenuItem down = new JMenuItem("Move down");
+      down.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          AbstractInsnNode node = ain.getNext();
+          mn.instructions.remove(node);
+          mn.instructions.insertBefore(ain, node);
+          OpUtils.clearLabelCache();
+          MyCodeList.this.loadInstructions(mn);
+        }
+      });
+      menu.add(down);
+      JMenuItem remove = new JMenuItem("Remove");
+      remove.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          mn.instructions.remove(ain);
+          OpUtils.clearLabelCache();
+          MyCodeList.this.loadInstructions(mn);
+        }
+      });
+      menu.add(remove);
+      addPopupListener(menu);
+      menu.show(jbm, (int) jbm.getMousePosition().getX(), (int) jbm.getMousePosition().getY());
+    }
+  }
+
+  protected void createPopupForEmptyList(JByteMod jbm) {
+    JPopupMenu menu = new JPopupMenu();
+    if (currentMethod != null) {
+      JMenuItem add = new JMenuItem("Add");
+      add.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          try {
+            EditDialogue.createInsertInsnDialog(currentMethod, null, true);
+          } catch (Exception e1) {
+            new ErrorDisplay(e1);
+          }
+
+        }
+      });
+      menu.add(add);
+    } else if (currentClass != null) {
+      JMenuItem add = new JMenuItem("Add");
+      add.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          try {
+            FieldNode fn = new FieldNode(1, "", "", "", null);
+            EditDialogueSpecial.createInsertDialogue(null, fn);
+            if (fn.signature.isEmpty()) {
+              fn.signature = null;
+            }
+            currentClass.fields.add(fn);
+          } catch (Exception e1) {
+            new ErrorDisplay(e1);
+          }
+          MyCodeList.this.loadFields(currentClass);
+        }
+      });
+      menu.add(add);
+    }
+    menu.show(jbm, (int) jbm.getMousePosition().getX(), (int) jbm.getMousePosition().getY());
+  }
+
+  protected void addPopupListener(JPopupMenu menu) {
+    menu.addPopupMenuListener(new PopupMenuListener() {
+      public void popupMenuCanceled(PopupMenuEvent popupMenuEvent) {
+        MyCodeList.this.setFocusable(true);
+      }
+
+      public void popupMenuWillBecomeInvisible(PopupMenuEvent popupMenuEvent) {
+        MyCodeList.this.setFocusable(true);
+      }
+
+      public void popupMenuWillBecomeVisible(PopupMenuEvent popupMenuEvent) {
+        MyCodeList.this.setFocusable(false);
       }
     });
   }
