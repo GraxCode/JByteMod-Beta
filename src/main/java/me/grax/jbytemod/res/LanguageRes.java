0d2bb69c1618bb364de515344a3854d40082839e
GraxCode <grax@graxco.de>
2018-01-28 14:02:11 +0100

    load default locale incase some translation is missing

--- src/main/java/me/grax/jbytemod/res/LanguageRes.java
+++ src/main/java/me/grax/jbytemod/res/LanguageRes.java
@@ -2,6 +2,7 @@ package me.grax.jbytemod.res;
 
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -16,21 +17,24 @@ import me.grax.jbytemod.utils.ErrorDisplay;
 
 public class LanguageRes {
   private final HashMap<String, String> map = new HashMap<>();
+  private final HashMap<String, String> defaultMap = new HashMap<>();
 
   public LanguageRes() {
     JByteMod.LOGGER.log("Reading Language XML..");
-    this.readXML();
+    this.readXML(map, getXML());
+    this.readXML(defaultMap, LanguageRes.class.getResourceAsStream("/locale/en.xml"));
+    JByteMod.LOGGER.log("Successfully loaded " +map.size() + " local resources and " + defaultMap.size() + " default resources");
   }
 
   public String getResource(String desc) {
-    return map.getOrDefault(desc, desc);
+    return map.getOrDefault(desc, defaultMap.getOrDefault(desc, desc));
   }
 
-  private void readXML() {
+  private void readXML(Map<String, String> m, InputStream is) {
     try {
       DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
       DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-      Document doc = dBuilder.parse(getXML());
+      Document doc = dBuilder.parse(is);
       doc.getDocumentElement().normalize();
       Element resources = doc.getDocumentElement();
       NodeList nodes = resources.getChildNodes();
@@ -38,12 +42,12 @@ public class LanguageRes {
         Node e = (Node) nodes.item(i);
         if (e.getNodeName().equals("string")) {
           Element el = (Element) e;
-          map.put(el.getAttribute("name"), e.getTextContent());
+          m.put(el.getAttribute("name"), e.getTextContent());
         }
       }
-      JByteMod.LOGGER.log("Successfully loaded " + map.size() + " resources");
     } catch (Exception e) {
       new ErrorDisplay(e);
+      JByteMod.LOGGER.err("Failed to load resources: " + e.getMessage());
     }
   }
 
@@ -52,6 +56,9 @@ public class LanguageRes {
     if (is == null) {
       JByteMod.LOGGER.warn("Locale not found, using default en.xml");
       is = LanguageRes.class.getResourceAsStream("/locale/en.xml");
+      if (is == null) {
+        JByteMod.LOGGER.err("en.xml not found!");
+      }
     }
     return is;
   }
