83e85d36cc493ec4499ba117f7e5c2bfa3215228
undefined <mymcpe42@Gmail.com>
2019-02-11 15:04:04 +0800

    Fix Attach Cause NPE
    http://prntscr.com/mjes8f

--- src/main/java/me/grax/jbytemod/ui/MyToolBar.java
+++ src/main/java/me/grax/jbytemod/ui/MyToolBar.java
@@ -51,7 +51,26 @@ public class MyToolBar extends JToolBar {
   }
 
   private ImageIcon getIcon(String string) {
-    return new ImageIcon(Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/resources/toolbar/" + string + ".png")));
+    		try {
+			return new ImageIcon(Toolkit.getDefaultToolkit()
+					.getImage(this.getClass().getResource("/resources/toolbar/" + string + ".png")));
+		} catch (Throwable x) {
+			x.printStackTrace();
+			System.out.println("[Error] Load ImageIcon Failed.Try using empty ImageIcon to instead!");
+			String emptyImage = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEAYABgAAD/4QBERXhpZgAATU0AKgAAAAgAAgENAAIAAAAWAAAAJgESAAMAAAABAAEAAAAAAAA8P1BIUCBQaHBpbmZvKCk7ID8+/xQA/9sAQwACAQECAQECAgICAgICAgMFAwMDAwMGBAQDBQcGBwcHBgcHCAkLCQgICggHBwoNCgoLDAwMDAcJDg8NDA4LDAwM/9sAQwECAgIDAwMGAwMGDAgHCAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwM/8AAEQgADAANAwEiAAIRAQMRAf/EAB8AAAEFAQEBAQEBAAAAAAAAAAABAgMEBQYHCAkKC//EALUQAAIBAwMCBAMFBQQEAAABfQECAwAEEQUSITFBBhNRYQcicRQygZGhCCNCscEVUtHwJDNicoIJChYXGBkaJSYnKCkqNDU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6g4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2drh4uPk5ebn6Onq8fLz9PX29/j5+v/EAB8BAAMBAQEBAQEBAQEAAAAAAAABAgMEBQYHCAkKC//EALURAAIBAgQEAwQHBQQEAAECdwABAgMRBAUhMQYSQVEHYXETIjKBCBRCkaGxwQkjM1LwFWJy0QoWJDThJfEXGBkaJicoKSo1Njc4OTpDREVGR0hJSlNUVVZXWFlaY2RlZmdoaWpzdHV2d3h5eoKDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uLj5OXm5+jp6vLz9PX29/j5+v/aAAwDAQACEQMRAD8A/fyiiigD/9k=";
+			byte[] b = null;
+			try {
+				b = new sun.misc.BASE64Decoder().decodeBuffer(emptyImage);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			for (int i = 0; i < b.length; ++i) {
+				if (b[i] < 0) {
+					b[i] += 256;
+				}
+			}
+			return new ImageIcon(b);
+		}
   }
 
   protected JButton makeNavigationButton(String action, ImageIcon i, ActionListener a) {
